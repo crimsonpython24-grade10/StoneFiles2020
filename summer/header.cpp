28a195d4fca0bfc4f38901ec15a877d734b8ccfd
Warren Wang <66010401+crimsonpython24-grade10@users.noreply.github.com>
2020-07-06 20:48:15 +0800

    Update header.cpp

--- summer/header.cpp
+++ summer/header.cpp
@@ -1,10 +1,10 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> // Globalman
 using namespace std;
-
+ 
 typedef long long ll;
 typedef const int ci;
 typedef vector<int> vi;
-
+ 
 #define MOD (ll)(1e9+7)
 #define INF (2e9)
 #define for0(i, x) for (ll i=0; i<((ll)x); ++i)
@@ -15,15 +15,15 @@ typedef vector<int> vi;
 #define sz(s) ((int)s.size())
 #define pub push_back
 #define pob pop_back
-#define in1(x) (cin >> &(x));
-#define in2(x, y) (cin >> &(x) >> &(y));
-#define in3(x, y, z) (cin >> &(x) >> &(y) >> &(z));
+#define in1(x) (cin >> (x));
+#define in2(x, y) (cin >> (x) >> (y));
+#define in3(x, y, z) (cin >> (x) >> (y) >> (z));
 #define pr(x) (cout << x);
 #define prl(x) (cout << x << endl); 
-
+ 
 template <class T> void minify(T & a, const T & b) { if (a > b) a = b; }
 template <class T> void maxify(T & a, const T & b) { if (a < b) a = b; }
-
+ 
 ll gcd (ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
 ll lcm (ll a, ll b) { return a/gcd(a, b) * b; }
 ll fpow(ll b, ll p) {
@@ -32,20 +32,20 @@ ll fpow(ll b, ll p) {
 		if(p & 1) ans = (ans * b) % MOD; p /= 2; b=(b * b) % MOD;
 	return ans;
 }
-
+ 
 void init() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 }
-
+ 
 // End of macros, code below this section
 
-// Define vars here (replace this line)
+// Variables
 
-// Other helper methods here (also replace)
+// Methods
 
 void solve() {
-
+	
 }
 
 int main() {
@@ -53,4 +53,3 @@ int main() {
 	solve();
 	return 0;
 }
-
