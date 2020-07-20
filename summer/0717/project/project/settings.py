a72f8df7ac99717bd5f5f6ebb0828b6748ad0d31
Warren Wang <warrenyjwang@gmail.com>
2020-07-20 22:38:33 +0800

    fixed api

--- summer/0717/project/project/settings.py
+++ summer/0717/project/project/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_SERIALIZER_CLASS': 'todo.pagination.CustomPaginationSerializer',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
 
--- summer/0717/project/todo/serializers.py
+++ summer/0717/project/todo/serializers.py
@@ -3,7 +3,7 @@ from .models import Todo
 from rest_framework import serializers
 
 
-class TodoSerializer(serializers.BaseSerializer):
+class TodoSerializer(serializers.ModelSerializer):
     level_choices = [
         ('important', 'Important'),
         ('normal', 'Normal'),
@@ -13,3 +13,8 @@ class TodoSerializer(serializers.BaseSerializer):
     desc = serializers.CharField(max_length=500, allow_blank=True)
     level = serializers.ChoiceField(choices=level_choices)
     created = serializers.DateTimeField()
+
+    class Meta:
+        model = Todo
+        fields = ['title', 'desc', 'level', 'created']
+        
